fd85474bbff0e97638dae2058dd51977a469cf44
Orlando Junior <orlandojsjunior@hotmail.com>
2025-04-14 22:22:03 -0300

    ADD - Adiciona conteúdo introdutório sobre listas em Python

---
+++ Nível_1/08 - Listas.py
@@ -0,0 +1,17 @@
+'''
+Nesta aula, vamos aprender sobre listas em Python. 
+
+- Uma lista é uma coleção de elementos ordenáveis e mutáveis. 
+- Podemos declarar uma lista usando colchetes e separando os elementos por vírgula. 
+- Os elementos podem ser de diferentes tipos. 
+
+Podemos:
+1. Acessar os elementos da lista usando índices, que começam em 0.
+2. Exibir a lista completa ou apenas um elemento específico usando a função `print`.
+3. Fatiar a lista, ou seja, obter um intervalo de elementos:
+    - Fatiar do começo até um elemento específico.
+    - Fatiar a partir de um elemento específico.
+
+Na próxima aula, veremos sobre atribuição e os principais métodos das listas.
+'''
+
