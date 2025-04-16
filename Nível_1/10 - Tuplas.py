2fe8f77dc0c155eaa643bf5ad1fcfe0c15aa54d8
Orlando Junior <orlandojsjunior@hotmail.com>
2025-04-16 00:47:55 -0300

    ADD - Adiciona conteúdo sobre tuplas , incluindo características e métodos úteis

---
+++ Nível_1/10 - Tuplas.py
@@ -0,0 +1,33 @@
+'''
+Principais características das tuplas:
+Nesta aula, vamos aprender sobre o próximo tipo de variável em Python: a tupla.
+
+- A tupla é uma coleção ordenada e imutável de elementos:
+    - Uma vez criada, não podemos modificar seus elementos.
+    - Podemos criar uma tupla usando parênteses e acessar seus elementos da mesma forma que uma lista.
+
+- Métodos úteis das tuplas:
+    1. `count`: Retorna a quantidade de vezes que um elemento aparece na tupla.
+    2. `index`: Retorna o índice da primeira ocorrência de um elemento na tupla.
+'''
+
+# Criando uma tupla
+
+minha_tupla = (1, 2, 2, 3, 4)
+
+print(f"Minha Tupla: ", minha_tupla) 
+print(f"Tipo: ", type(minha_tupla)) 
+print(f"Acessando o elemento no índice 0: ", minha_tupla[0]) # Acessando o primeiro elemento da tupla
+print(f"Acessando o último elemento da tupla: ", minha_tupla[-1]) # Acessando o último elemento usando índice negativo
+
+
+# Metodo count().
+Contagem = minha_tupla.count(2) # Contando quantas vezes o número 2 aparece na tupla
+
+print(f"Quantidade de vezes que o elemento 2 aparece na tupla: ", Contagem) # Exibindo a contagem do número 2 na tupla
+print(f"Quantidade de vezes que o elemento 2 aparece na tupla: ", minha_tupla.count(2)) # Contando quantas vezes o número 2 aparece na tupla
+
+# Metodo index().
+Indice = minha_tupla.index(3) # Encontrando o índice da primeira ocorrência do número 2 na tupla
+print(f"Índice do elemento 3 na tupla: ", Indice) # Exibindo o índice do número 3 na tupla 
+print(f"Índice do elemento 3 na tupla: ", minha_tupla.index(3)) # Encontrando o índice da primeira ocorrência do número 3 na tupla
