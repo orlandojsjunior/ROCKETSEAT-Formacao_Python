f40cda08654af01c7b4c9367a4c2293ef6e4c66e
Orlando Junior <orlandojsjunior@hotmail.com>
2025-04-16 01:28:53 -0300

    UPDATE - Adiciona quebras de linha para melhor legibilidade no código de listas

--- Modulo_01/08 - Listas.py
+++ Modulo_01/08 - Listas.py
@@ -17,9 +17,15 @@ Na próxima aula, veremos sobre atribuição e os principais métodos das listas
 '''
 
 minha_lista = [1, 2, 3, 4, 5, "Orlando", True, False]
+
 print(minha_lista)  # Exibe a lista completa
+
 print("Exibindo elementos em uma lista:", minha_lista[0])  # Exibe o primeiro elemento da lista
+
 print("Exibindo elementos em uma lista:", minha_lista[5])  # Exibe o quinto elemento da lista
+
 print("Exibindo fatiamento da lista:", minha_lista[1:7])  # Exibe os elementos a partir do segundo até o final da lista - Fatiamento de lista
+
 print("Exibindo fatiamento da lista:", minha_lista[:6])  # Exibe os elementos do início até o sexto elemento
+
 print("Exibindo fatiamento da lista:", minha_lista[3:])  # Exibe os elementos a partir do terceiro elemento até o final da lista
