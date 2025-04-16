99fb7e40cd4854e2fc8a5d487d02c8bcb003523d
Orlando Junior <orlandojsjunior@hotmail.com>
2025-04-16 00:56:36 -0300

    UPDATE - Melhora a formatação das saídas de impressão e corrige comentários no código sobre tuplas

--- Nível_1/10 - Tuplas.py
+++ Nível_1/10 - Tuplas.py
@@ -15,19 +15,19 @@ Nesta aula, vamos aprender sobre o próximo tipo de variável em Python: a tupla
 
 minha_tupla = (1, 2, 2, 3, 4)
 
-print(f"Minha Tupla: ", minha_tupla) 
-print(f"Tipo: ", type(minha_tupla)) 
-print(f"Acessando o elemento no índice 0: ", minha_tupla[0]) # Acessando o primeiro elemento da tupla
-print(f"Acessando o último elemento da tupla: ", minha_tupla[-1]) # Acessando o último elemento usando índice negativo
-
+print(f"Minha Tupla: {minha_tupla}")
+print(f"Tipo: {type(minha_tupla)}")
+print(f"Acessando o elemento no índice 0: {minha_tupla[0]}")  # Acessando o primeiro elemento da tupla
+print(f"Acessando o último elemento da tupla: {minha_tupla[-1]}")  # Acessando o último elemento usando índice negativo
 
 # Metodo count().
-Contagem = minha_tupla.count(2) # Contando quantas vezes o número 2 aparece na tupla
 
-print(f"Quantidade de vezes que o elemento 2 aparece na tupla: ", Contagem) # Exibindo a contagem do número 2 na tupla
-print(f"Quantidade de vezes que o elemento 2 aparece na tupla: ", minha_tupla.count(2)) # Contando quantas vezes o número 2 aparece na tupla
+Contagem = minha_tupla.count(2)  # Contando quantas vezes o número 2 aparece na tupla
+print(f"Quantidade de vezes que o elemento 2 aparece na tupla: {Contagem}")
+print(f"Quantidade de vezes que o elemento 2 aparece na tupla: {minha_tupla.count(2)}")
 
 # Metodo index().
-Indice = minha_tupla.index(3) # Encontrando o índice da primeira ocorrência do número 2 na tupla
-print(f"Índice do elemento 3 na tupla: ", Indice) # Exibindo o índice do número 3 na tupla 
-print(f"Índice do elemento 3 na tupla: ", minha_tupla.index(3)) # Encontrando o índice da primeira ocorrência do número 3 na tupla
+
+Indice = minha_tupla.index(3)  # Encontrando o índice da primeira ocorrência do número 3 na tupla
+print(f"Índice do elemento 3 na tupla: {Indice}")
+print(f"Índice do elemento 3 na tupla: {minha_tupla.index(3)}")
