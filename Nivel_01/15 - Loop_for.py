f7589923a9bb42671dbc0fad8d79ecbdabd4f4d0
Orlando Junior <orlandojsjunior@hotmail.com>
2025-04-17 23:51:56 -0300

    UPDATE - Teste #TUDO no vscode

--- Nivel_01/15 - Loop_for.py
+++ Nivel_01/15 - Loop_for.py
@@ -20,12 +20,12 @@ for elemento in lista:
   print(elemento)
 
 
-print("For utilizando tupla")
+print("For utilizando tupla") #TODO: teste 01
 tupla = (1, 2, 3, 4, 5)
 for elemento in tupla:
   print(elemento)
 
-'''
+''' 
 pessoa = {"nome": "João", "idade": 30, "cidade": "São Paulo"}
 print("For utilizando dicionatio - chaves")
 for chave in pessoa.keys():
