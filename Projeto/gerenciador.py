f6ad87f3137a4e3f6cb538acdf30eba562d424d1
Orlando Junior <orlandojsjunior@hotmail.com>
2025-04-23 19:12:32 -0300

    ADD - Funções  no projeto - 1. Adicionar tarefa

--- Projeto/gerenciador.py
+++ Projeto/gerenciador.py
@@ -1,18 +1,34 @@
-# criando um menu para um gerenciador de lista de tarefas. 
-# O loop while será usado para repetir o menu até que o usuário escolha a opção de sair.
+# criando um menu para um gerenciador de lista de tarefas.
+# Funções no projeto - Adicionando tarefas
 
+
+def adicionar_tarefa(tarefas, nome_tarefa):
+    
+    tarefa = {"tarefa": nome_tarefa, "completada": False}
+    tarefas.append(tarefa)
+    print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
+    return
+
+
+
+tarefas = [] # Lista onde as tarefas serão armazenadas
+
+# Menu do Gerenciador de lista de tarefas
 while True:
-    print("\nMenu do Gerenciador de Lista de tarefas:")
-    print("1. Adicionar tarefa")
-    print("2. Ver tarefas")
-    print("3. Atualizar Tarefas")
-    print("4. Completar Tarefas")
-    print("5. Deletar tarefas completadas")
-    print("6. Sair")
-
-    escolha = input("Digite a sua escolha: ")
-
-    if escolha == "6":
-        break
-
+   print("\nMenu do Gerenciador de Lista de tarefas:")
+   print("1. Adicionar tarefa")
+   print("2. Ver tarefas")
+   print("3. Atualizar Tarefas")
+   print("4. Completar Tarefas")
+   print("5. Deletar tarefas completadas")
+   print("6. Sair")
+ 
+   escolha = input("Digite uma opção do menu: ")
+ 
+   if escolha == "1":
+     nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
+     adicionar_tarefa(tarefas, nome_tarefa)
+   elif escolha == "6":
+     break
+ 
 print("Programa finalizado")
