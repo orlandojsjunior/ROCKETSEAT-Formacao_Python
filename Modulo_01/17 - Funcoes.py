49123e17edbca04d5614dbbd4cba0a370364edeb
Orlando Junior <orlandojsjunior@hotmail.com>
2025-04-23 17:59:29 -0300

    UPDATE - Ajusta a formatação e a apresentação dos resultados das funções

--- Modulo_01/17 - Funcoes.py
+++ Modulo_01/17 - Funcoes.py
@@ -3,23 +3,30 @@
 def saudacao(nome):
     print(f"Olá, {nome}!")
 
+
 print("\nChamando a função saudacao:")
 saudacao("Alice")
 saudacao("Bob")
 
 # Função com retorno
+
+
 def quadrado(numero):
     resultado = numero ** 2
     return resultado
 
+
 print("\nChamando função quadrado:")
 resultado_quadrado = quadrado(6)
-print("Resultado da função quadrado:", resultado_quadrado)
+print(f"Resultado da função quadrado: {resultado_quadrado}")
 
 # Função com múltiplos parâmetros
+
+
 def soma(numero1, numero2):
     resultado = numero1 + numero2
     return resultado
+
 
 print("\nChamando a função soma:")
 numero1 = 25
