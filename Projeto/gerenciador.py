50a742881f38f9303323b558bde3f31c58a557ac
Orlando Junior <orlandojsjunior@hotmail.com>
2025-04-26 18:33:49 -0300

    ADD - Funções do projeto - 3. Atualizar tarefas

--- Projeto/gerenciador.py
+++ Projeto/gerenciador.py
@@ -1,6 +1,7 @@
 # criando um menu para um gerenciador de lista de tarefas.
 # Funções no projeto - Adicionando tarefas
 # Funções do projeto - Visualizando tarefas
+# Funções do projeto - Atualizar tarefas
 
 
 def adicionar_tarefa(tarefas, nome_tarefa):
@@ -18,8 +19,17 @@ def ver_tarefas(tarefas):
         nome_tarefa = tarefa["tarefa"]
         print(f"{indice}. [{status}] {nome_tarefa}")
 
+def atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome_tarefa):
+   indice_tarefa_ajustado = int(indice_tarefa) - 1
+   if indice_tarefa_ajustado >= 0 and indice_tarefa_ajustado < len(tarefas):
+     tarefas[indice_tarefa_ajustado]["tarefa"] = novo_nome_tarefa
+     print(f"Tarefa {indice_tarefa} atualizada para {novo_nome_tarefa}")
+   else:
+     print("Índice de tarefas inválido.")
+   return
 
-tarefas = []  # Lista onde as tarefas serão armazenadas
+
+tarefas = [] # Lista onde as tarefas serão armazenadas
 
 # Menu do Gerenciador de lista de tarefas
 while True:
@@ -38,6 +48,11 @@ while True:
         adicionar_tarefa(tarefas, nome_tarefa)
     elif escolha == "2":
         ver_tarefas(tarefas)
+    elif escolha == "3":
+     ver_tarefas(tarefas)
+     indice_tarefa = input("Digite o número da tarefa que deseja atualizar: ")
+     novo_nome = input("Digite o novo nome da tarefa: ")
+     atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome)
     elif escolha == "6":
         break
     
